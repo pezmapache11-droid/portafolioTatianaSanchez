772bcbe822f6b198251f209b588397dd84789353
pezmapache11-droid <pezmapache.11@gmail.com>
2025-11-14 11:03:20 -0500

    Add files via upload

---
+++ portafolioTatianaSanchez-main/portafolioTatianaSanchez-main/portafolioTatianaSanchez-main/Tatiana Empresa (El que me borre esto le pego)/que eres script.js
@@ -0,0 +1,7 @@
+document.getElementById("designerBtn").addEventListener("click", function() {
+    alert("Has elegido: Diseñador 🎨");
+});
+
+document.getElementById("userBtn").addEventListener("click", function() {
+    alert("Has elegido: Usuario 👤");
+});
