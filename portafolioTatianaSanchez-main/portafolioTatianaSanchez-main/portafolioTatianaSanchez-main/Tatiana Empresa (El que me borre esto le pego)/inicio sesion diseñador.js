6b761fa71bfc358435f496b69f027ba437b44e09
pezmapache11-droid <pezmapache.11@gmail.com>
2025-11-18 12:37:38 -0500

    Delete portafolioTatianaSanchez-main directory

--- portafolioTatianaSanchez-main/portafolioTatianaSanchez-main/portafolioTatianaSanchez-main/Tatiana Empresa (El que me borre esto le pego)/inicio sesion diseñador.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".input-box input").forEach(input => {
-    input.addEventListener("focus", () => {
-        input.parentElement.style.background = "rgba(255,255,255,0.45)";
-    });
-
-    input.addEventListener("blur", () => {
-        input.parentElement.style.background = "rgba(255,255,255,0.3)";
-    });
-});
