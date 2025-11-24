8bc669378bc9a801c06e145dbe4880e11d284004
pezmapache11-droid <pezmapache.11@gmail.com>
2025-11-24 12:48:48 -0500

    Delete Tatiana Empresa (El que me borre esto le pego) directory

--- Tatiana Empresa (El que me borre esto le pego)/que eres script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("designerBtn").addEventListener("click", function() {
-    alert("Has elegido: Diseñador 🎨");
-});
-
-document.getElementById("userBtn").addEventListener("click", function() {
-    alert("Has elegido: Usuario 👤");
-});
